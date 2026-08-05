fa27c5053ccae7663825b86578444e56f3499ff1
Itachposadas <posadas16.ap@gmail.com>
2026-08-05 17:56:37 -0600

    Primer commit del proyecto Flask

--- app.py
+++ app.py
@@ -1,31 +1,10 @@
-from flask import Flask
-from config import db, Config
-import os
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-app.config.from_object(Config)
-
-
-db.init_app(app)
-
-
-
-from routes.auth import auth
-app.register_blueprint(auth)
-
-
-
 @app.route("/")
-def index():
-    return "Tienda de ropa funcionando"
-
-
+def inicio():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT",5000))
-    )
+    app.run(debug=True)
