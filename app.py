554357654de9327b30052ff5769dc2011396eb28
Itachposadas <posadas16.ap@gmail.com>
2026-08-05 10:49:49 -0600

    Corregir configuracion SQLAlchemy

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask
-from config import db
+from config import db, Config
+import os
+
 
 app = Flask(__name__)
+
+
+app.config.from_object(Config)
+
 
 db.init_app(app)
 
 
+
 from routes.auth import auth
 app.register_blueprint(auth)
+
 
 
 @app.route("/")
@@ -15,5 +23,9 @@ def index():
     return "Tienda de ropa funcionando"
 
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT",5000))
+    )
